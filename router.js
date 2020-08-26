e91ce2b2c87ce675e796d59a617d9685612c0919
Richard Perrotta <perrottarichard@gmail.com>
2020-08-26 16:35:14 +0700

    stupid ws

--- router.js
+++ router.js
@@ -3,6 +3,8 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
   res.send({ response: "Server is running." }).status(200);
 });
 
