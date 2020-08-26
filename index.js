4f6aca8424f36851e08123b66e95219d608b1241
Richard Perrotta <perrottarichard@gmail.com>
2020-08-26 11:00:11 +0700

    cors problem

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 
 const app = express()
 var allowCrossDomain = function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'example.com');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
 
