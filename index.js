d2dc3e20141560daf49fe876c45b766799b9095b
Richard Perrotta <perrottarichard@gmail.com>
2020-08-26 09:32:25 +0700

    add procfile

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const socketio = require('socket.io')
 const http = require('http')
 const router = require('./router')
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users')
-const { callbackify } = require('util')
 
 const app = express()
+app.use(cors())
 
 const PORT = process.env.PORT || 5000
 
@@ -18,8 +19,8 @@ io.on('connection', (socket) => {
   socket.on('join', ({ name, room }) => {
     const { user, error } = addUser({ id: socket.id, name, room })
     if (user) {
-      socket.emit('message', { user: 'admin', text: `${user.name}, welcome to ${user.room}` })
-      socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined` })
+      socket.emit('message', { user: 'robot', text: `hi ${user.name}, welcome to ${user.room}` })
+      socket.broadcast.to(user.room).emit('message', { user: 'robot', text: `${user.name} has joined` })
       socket.join(user.room)
       io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) })
     }
@@ -36,7 +37,7 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     const user = removeUser(socket.id)
     if (user) {
-      io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left` })
+      io.to(user.room).emit('message', { user: 'robot', text: `${user.name} has left` })
     }
   })
 })
