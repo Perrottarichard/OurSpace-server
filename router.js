17a358ce23c69aaf7de2031c6ec7ba47327e3379
Richard Perrotta <perrottarichard@gmail.com>
2020-08-28 14:38:46 +0700

    fixes

--- router.js
+++ router.js
@@ -3,8 +3,6 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
   res.send({ response: "Server is running." }).status(200);
 });
 
