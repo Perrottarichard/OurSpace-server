7fe9c78197a7d8b7687d840c129a01a42cec366b
Richard Perrotta <perrottarichard@gmail.com>
2020-08-28 20:52:42 +0700

    updates

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ app.use(router)
 const server = http.createServer(app)
 
 
-const io = require("socket.io")(server);
-io.origins(['https://r-space.netlify.app/'])
+const io = require("socket.io")(server, { origins: 'https://r-space.netlify.app' });
 
 
 const PORT = process.env.PORT || 5000
@@ -24,16 +23,21 @@ server.listen(PORT, () => {
 })
 
 io.on('connection', (socket) => {
-  console.log('ws connected')
+  console.log('new client connected')
+
 
   socket.on('join', ({ name, room }) => {
-    const { user } = addUser({ id: socket.id, name, room })
+    if (name && room) {
+      const { user } = addUser({ id: socket.id, name, room })
+      socket.join(user.room)
+      socket.emit('message', { user: 'robot', text: `hi ${user.name}, welcome to ${user.room}` })
+      socket.broadcast.to(user.room).emit('message', { user: 'robot', text: `${user.name} has joined` })
 
-    socket.join(user.room)
-    socket.emit('message', { user: 'robot', text: `hi ${user.name}, welcome to ${user.room}` })
-    socket.broadcast.to(user.room).emit('message', { user: 'robot', text: `${user.name} has joined` })
+      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) })
+    } else {
+      window.alert('Please reload the page')
+    }
 
-    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) })
   })
 
   socket.on('sendMessage', (message) => {
